2440b4dd35363c3622bbbbc18d05fb6be4886e3a
Preston-Beck <pgbeck@lakeheadu.ca>
2021-12-02 11:43:25 -0500

    Added JSON FileReader to APIUtility.java

--- src/main/java/com/example/comp1011assignment2/Utilities/APIUtility.java
+++ src/main/java/com/example/comp1011assignment2/Utilities/APIUtility.java
@@ -1,4 +1,22 @@
 package com.example.comp1011assignment2.Utilities;
 
+import com.example.comp1011assignment2.Model.NHLStatsApiResponse;
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
+import java.io.FileReader;
+
 public class APIUtility {
+    public static NHLStatsApiResponse getTeamsFromJSON() {
+        Gson gson = new Gson();
+        NHLStatsApiResponse result = null;
+        try (FileReader fileReader = new FileReader("apiResponse.json");
+             JsonReader jsonReader = new JsonReader(fileReader)) {
+            result = gson.fromJson(jsonReader, NHLStatsApiResponse.class);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 }
